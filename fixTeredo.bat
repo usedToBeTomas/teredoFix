5eda3d5b6a4235796226e5e416aaa445c0fbd3ea
usedToBeTomas <85880657+usedToBeTomas@users.noreply.github.com>
2022-11-15 21:07:37 +0100

    First upload

---
+++ fixTeredo.bat
@@ -0,0 +1,13 @@
+netsh int teredo set state type=enterpriseclient
+netsh int teredo set state refreshinterval=20
+netsh int teredo set state clientport=52586
+sc config iphlpsvc start=auto
+sc config PolicyAgent start=auto
+net stop iphlpsvc
+net start iphlpsvc
+net stop PolicyAgent
+net start PolicyAgent
+netsh int teredo show state
+echo Computer will reboot in 30 seconds press any key to continue...
+pause
+shutdown -r -t 30
